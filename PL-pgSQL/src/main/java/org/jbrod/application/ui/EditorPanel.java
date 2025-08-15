9bf555322dd92883f95c0d97f9605267cbd357b1
Jbrod5 <jorgebravo202131782@cunoc.edu.gt>
2025-08-15 13:27:04 -0600

    Reemplazo de TextArea por CodeArea (soporte numeracion de linea)

--- PL-pgSQL/src/main/java/org/jbrod/application/ui/EditorPanel.java
+++ PL-pgSQL/src/main/java/org/jbrod/application/ui/EditorPanel.java
@@ -1,30 +1,33 @@
 package org.jbrod.application.ui;
 
-import javafx.application.Platform;
+
 import javafx.geometry.Insets;
 import javafx.scene.control.*;
-import javafx.scene.input.MouseButton;
-import javafx.scene.layout.*;
-import javafx.scene.text.Font;
 
-import java.io.IOException;
+import javafx.scene.layout.*;
+import org.fxmisc.flowless.VirtualizedScrollPane;
+import org.fxmisc.richtext.CodeArea;
+import org.fxmisc.richtext.LineNumberFactory;
+
+
 import java.nio.file.*;
-import java.util.HashMap;
-import java.util.Map;
+
 
 
 import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.StandardOpenOption;
+
 
 /**
- * Panel con TreeView (izq) y TabPane (der). Pasa un Path raíz al constructor.
+ * Panel con el editor de texto correspondiente al archivo.
  */
 public class EditorPanel extends BorderPane {
 
     private TextArea textArea;
+    private CodeArea codeArea;
+    //para control de linea y columna
+    private int line;
+    private int column;
+
     private Button btnGuardar;
     private Button btnEjecutar;
 
@@ -33,13 +36,18 @@ public class EditorPanel extends BorderPane {
     public EditorPanel(File file) {
         this.file = file;
 
-        // Crear TextArea
-        textArea = new TextArea();
-        textArea.setFont(Font.font("Monospaced", 14));
+        // Crear TextArea para el codigo (puede ser un CodeArea?)
+        //textArea = new TextArea();
+        //textArea.setFont(Font.font("Monospaced", 14));
+        codeArea = new CodeArea(); //Editor de codigo de la libreria RichTextFX
+        codeArea.setParagraphGraphicFactory(LineNumberFactory.get(codeArea)); //Agregar numeracion de linea
+        VirtualizedScrollPane<CodeArea> scrollForCodeArea = new VirtualizedScrollPane<>(codeArea);
+
 
         // Crear botones
         btnGuardar = new Button("Guardar");
         btnEjecutar = new Button("Ejecutar");
+
 
         // Contenedor horizontal para los botones
         HBox buttonBox = new HBox(10); // 10px de espacio
@@ -47,7 +55,9 @@ public class EditorPanel extends BorderPane {
         buttonBox.getChildren().addAll(btnGuardar, btnEjecutar);
 
         // Asignar posiciones en BorderPane
-        this.setCenter(textArea);
+        //this.setCenter(textArea);
+        //this.setCenter(codeArea); El scroll del codeArea no es comodo
+        this.setCenter(scrollForCodeArea);
         this.setBottom(buttonBox);
     }
 
